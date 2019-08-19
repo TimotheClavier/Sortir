e5a716c271092cd6d5413bc8e847a5a3259255fb
Maxime <schweitzer.maxime.06@gmail.com>
2019-08-19 16:36:36 +0200

    CRUD : City, Place, Situation, trip + remember_me

---
+++ src/Form/TripType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Trip;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TripType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('tripDate')
+            ->add('inscriptionDate')
+            ->add('seat')
+            ->add('duration')
+            ->add('description')
+            ->add('organizer')
+            ->add('status')
+            ->add('cause')
+            ->add('place')
+            ->add('users')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Trip::class,
+        ]);
+    }
+}
